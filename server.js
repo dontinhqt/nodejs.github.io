4aa577968f23a792a6fa169f1f63bcd00b4a757f
mrvinhdv <duongvinh.itdv3@gmail.com>
2017-08-06 11:58:03 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,77 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    methodOverride = require('method-override'),
+    session = require('express-session'),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    fs = require('fs'),
+    mongoose = require('mongoose'),
+    morgan = require('morgan');
+
+var app = module.exports = express();
+
+//var passport = require('passport');
+var config = require('./config/database');
+
+
+
+// all environments
+app.set('port', process.env.PORT || 8000);
+app.use(session({
+    secret: '2C44-4D44-WppQ38S',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
+//user passport session and express session
+//app.use(passport.initialize());
+//app.use(passport.session());
+app.use(methodOverride());
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+//passport serialize a User
+//passport.serializeUser(function(user, done) {
+//    done(null, user);
+//});
+
+// error handling middleware should be loaded after the loading the routes
+if ('development' == app.get('env')) {
+    app.use(errorHandler());
+}
+
+// log to console
+app.use(morgan('dev'));
+
+
+
+var server = http.createServer(app);
+
+mongoose.connect(config.database);
+//passport authenticate
+//require('./config/passport')(passport);
+
+var db = mongoose.connection;
+fs.readdirSync('./apis').forEach(function (file) {
+    if (file.substr(-3) == '.js') {
+        var route = require('./apis/' + file);
+        route.controller(app);
+    }
+});
+
+
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+    console.log("open db successfully");
+});
+
+server.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
