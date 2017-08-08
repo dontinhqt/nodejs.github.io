38bfceabfd36c2ea42a20d4b765aa07a097ea9ca
mrvinhdv <duongvinh.itdv3@gmail.com>
2017-08-08 17:18:43 +0700

    update api replace data

--- apis/api_services.js
+++ apis/api_services.js
@@ -161,7 +161,7 @@ module.exports.controller = function (app) {
         if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
             where = {_id: req.params.id};
         } else {
-            where = { $text: { $search: req.params.id }};
+            where = {$text: {$search: req.params.id}};
         }
         Service.findOne(where).exec(function (err, service) {
             if (err) {
@@ -294,5 +294,90 @@ module.exports.controller = function (app) {
 
     });
 
+    //api check exit by id
+    app.get('/api/v1/services/:id/exits', function (req, res) {
+        if (!req.params.id) {
+            res.status(404);
+        }
+        Service.findOne({_id: req.params.id}).exec(function (err, service) {
+            if (err) {
+                res.status(500);
+            }
+            var isValid = true;
+            if (!service) {
+                isValid = false;
+            }
+            res.status(200).json(isValid);
+        });
+    });
 
+    //api replace attributes
+    app.post('/api/v1/services/:id/replace',function(req,res){
+        if (!req.params.id) {
+            res.status(404).json({'msg': 'Request not fount'});
+        }
+
+        var service_name = req.body.service_name ? req.body.service_name : '';
+        var active = true;
+        var prices = req.body.price ? req.body.price : new Array();
+        var updates = req.body.update ? req.body.update : new Array();
+        var executes = req.body.execute ? req.body.execute : new Array();
+        var group = req.body.group ? req.body.group : '';
+        var type = req.body.type ? req.body.type : '';
+        var priceArr = new Array();
+        if (prices) {
+            for (var i = 0; i < prices.length; i++) {
+                var price_origin = prices[i].price_origin ? prices[i].price_origin : 0;
+                var first_price = prices[i].first_price ? prices[i].first_price : 0;
+                var second_price = prices[i].second_price ? prices[i].second_price : 0;
+                var isCover = prices[i].isCover ? prices[i].isCover : false;
+                var isDeductible = prices[i].isDeductible ? prices[i].isDeductible : false;
+                var isDiscount = prices[i].isDiscount ? prices[i].isDiscount : false;
+                priceArr.push({
+                    price_origin: price_origin, first_price: first_price, second_price: second_price,
+                    isCover: isCover, isDeductible: isDeductible, isDiscount: isDiscount
+                });
+            }
+        }
+
+        var updateArr = new Array();
+        if (updates.length > 0) {
+            for (var i = 0; i < updates.length; i++) {
+                var time = updates[i].time ? updates[i].time : 0;
+                var updateBy = updates[i].updateBy ? updates[i].updateBy : 0;
+                updateArr.push({
+                    time: time, updateBy: updateBy
+                });
+            }
+        }
+
+        var executeArr = new Array();
+        if (executes.length > 0) {
+            for (var i = 0; i < executes.length; i++) {
+                var name = executes[i].name ? executes[i].name : "";
+                executeArr.push({
+                    name: name
+                });
+            }
+        }
+
+        Service.findOne({_id: req.params.id}).exec(function (err, service) {
+            if (err) {
+                res.status(500);
+            }
+            service.service_name = service_name;
+            service.active = active;
+            service.price = priceArr;
+            service.update = updateArr;
+            service.execute = executeArr;
+            service.group = group;
+            service.type = type;
+            service.save(function (err, resp) {
+                if (err) {
+                    res.status(500);
+                }
+                res.status(200).json({msg: 'Updated successfully!'});
+            });
+        });
+    });
 };
